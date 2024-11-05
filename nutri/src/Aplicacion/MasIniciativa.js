3400ef4100a98da47daaa965b2a94bd89ce87384
SteelFenix09 <f_771@outlook.com>
2024-11-05 07:36:41 -0600

    Informacion iniciativaa

---
+++ nutri/src/Aplicacion/MasIniciativa.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Diseño/img/Iniciativa.css'
+
+function MasIniciativa() {
+    return (
+        <div>
+            <h1 className='tit-inici'>Recomendaciones y Tips</h1>
+            <table>
+                <tr>
+                    <td>Hola 1</td>
+                    <td>Hola 2</td>
+                    <td>Hola 3</td>
+                    <td>Hola 4</td>
+                </tr>
+                <tr>
+                    <td>Hola 5</td>
+                    <td>Hola 6</td>
+                    <td>Hola 7</td>
+                    <td>Hola 8</td>
+                </tr>
+                <tr>
+                    <td>Hola 9</td>
+                    <td>Hola 10</td>
+                    <td>Hola 11</td>
+                    <td>Hola 12</td>
+                </tr>
+                <tr>
+                    <td>Hola 13</td>
+                    <td>Hola 14</td>
+                    <td>Hola 15</td>
+                    <td>Hola 16</td>
+                </tr>
+                <tr>
+                    <td>Hola 17</td>
+                    <td>Hola 18</td>
+                    <td>Hola 19</td>
+                    <td>Hola 20</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
+
+export default MasIniciativa
--- nutri/src/Aplicacion/Rut/Rutas.js
+++ nutri/src/Aplicacion/Rut/Rutas.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import Iniciativa from '../Diseño/Iniciativa'
+import MasIniciativa from '../MasIniciativa'
 
 
 function Rutas() {
     return (
         <Routes>
             <Route path='/iniciativa' element = {<Iniciativa/>} />
+            <Route path='/mas' element = {<MasIniciativa/>}/>
         </Routes>
     )
 }
