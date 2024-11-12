87eee086f0906e8182d67d6181e7966cd1d5aa60
SteelFenix09 <f_771@outlook.com>
2024-11-11 18:25:23 -0600

    Listo

--- nutri/src/Aplicacion/MasIniciativa.js
+++ nutri/src/Aplicacion/MasIniciativa.js
@@ -6,39 +6,46 @@ function MasIniciativa() {
         <div>
             <h1 className='tit-inici'>¡Objetivos y Indicaciones! </h1>
             <table className='text1'>
-                <tr>
-                <td> <h3> ¡Objetivos! </h3><hr></hr></td>
-                <td></td><td></td>
-                <td><h3> ¡Indicaciones! </h3><hr></hr></td>
-                 </tr>
-                
-                <tr >
-                    <td> - Facilitar el acceso a información nutricional </td>
-                    <td></td><td></td>
-                    <td> - Límite de consumo de azúcar </td>
-                </tr>
+                <thead>
+                    <tr>
+                        <th><h3>Objetivos</h3><hr></hr></th>
+                        <th id="espacio"></th>
+                        <th><h3>Indicaciones</h3><hr></hr></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>- Facilitar el acceso a información nutricional</td>
+                        <th id="espacio"></th>
+                        <td>- Límite de consumo de azúcar</td>
+                    </tr>
 
-                <tr>
-                    <td> - Personalización según necesidades de salud </td>
-                    <td>  </td>
-                    <td> - Alertas sobre grasas saturadas y trans    </td>
-                </tr>
-                <tr>
-                    <td> - Promover hábitos de alimentación saludable </td>
-                    <td></td><td></td><td> - Guía de consumo de sodio</td>
-                </tr>
-                <tr>
-                    <td> - Comparación de productos </td>
-                    <td></td><td></td><td> - Promoción de alimentos ricos en fibra</td>
-                </tr>
-                <tr>
-                    <td> - Informar sobre alérgenos e ingredientes críticos </td>
-                    <td></td><td></td><td> - Alerta de calorías vacías </td>
-                </tr>
-                <tr>
-                    <td> - Apoyo para objetivos de dieta específicos </td>
-                    <td></td><td></td><td> - Alternativas saludables</td>
-                </tr>
+                    <tr>
+                        <td>- Personalización según necesidades de salud</td>
+                        <th id="espacio"></th>
+                        <td>- Alertas sobre grasas saturadas y trans</td>
+                    </tr>
+                    <tr>
+                        <td>- Promover hábitos de alimentación saludable</td>
+                        <th id="espacio"></th>
+                        <td>- Guía de consumo de sodio</td>
+                    </tr>
+                    <tr>
+                        <td>- Comparación de productos</td>
+                        <th id="espacio"></th>
+                        <td>- Promoción de alimentos ricos en fibra</td>
+                    </tr>
+                    <tr>
+                        <td>- Informar sobre alérgenos e ingredientes críticos</td>
+                        <th id="espacio"></th>
+                        <td>- Alerta de calorías vacías</td>
+                    </tr>
+                    <tr>
+                        <td>- Apoyo para objetivos de dieta específicos</td>
+                        <th id="espacio"></th>
+                        <td>- Alternativas saludables</td>
+                    </tr>
+                </tbody>
             </table>
         </div>
     )
